9bce4ce189a4c2ecb5b6c9dee0fd2b6f3c5c73cc
LicrimoVor <mr.licrimovor@yandex.ru>
2023-12-08 18:54:23 +0800

    больше код стайлу

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,28 @@
 import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 /** Роутер  */
-export const AppRouter = () => (
-    <Suspense fallback={<div>Загрузонька...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
-                        <div className="page-wrapper">
-                            {element}
-                        </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
-);
+export const AppRouter = () => {
+    const { t } = useTranslation();
+
+    return (
+        <Suspense fallback={<div>{t('loading')}</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={(
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        )}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -5,7 +5,6 @@ import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { useTheme, Theme } from 'app/providers/ThemeProvider';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
   className?: string
@@ -17,7 +16,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
 
     return (
         <Button
-            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={hundlerTheme}
             theme={ThemeButton.CLEAR}
         >
